0bed5b14faa024c914d2efbd1c8f27afa7caa8ee
ramiroSicilia <moto2.belgrano@gmail.com>
2026-04-02 13:51:18 -0300

    agrego sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://proyecto-freelance-next.vercel.app/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://proyecto-freelance-next.vercel.app/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://proyecto-freelance-next.vercel.app/shifts",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://proyecto-freelance-next.vercel.app/projects",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://proyecto-freelance-next.vercel.app/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
